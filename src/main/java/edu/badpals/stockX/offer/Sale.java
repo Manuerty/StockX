4a3d7a0b00dc5fdc431267201b2f3a769c9b1e7c
Manuerty <manuelcendonr@gmail.com>
2024-03-05 13:55:35 +0100

    feat (src) create the Sale class

--- src/main/java/edu/badpals/stockX/offer/Sale.java
+++ src/main/java/edu/badpals/stockX/offer/Sale.java
@@ -1,6 +1,27 @@
 package edu.badpals.stockX.offer;
 
-public class Sale {
-        private String size;
-        private Integer price;
+public class Sale implements Offer{
+        private String size="";
+        private Integer price=0;
+
+        Sale (String size, Integer price){
+                this.size = size;
+                this.price = price;
+        }
+        @Override
+        public String size(){
+                return this.size;
+        }
+        @Override
+        public int value(){
+                return this.price;
+        }
+        @Override
+        public int compareTo(Offer price){
+                return this.price.compareTo(price.value());
+        }
+        @Override
+        public String toString(){
+                return this.size() + Integer.toString(this.value());
+        }
 }
