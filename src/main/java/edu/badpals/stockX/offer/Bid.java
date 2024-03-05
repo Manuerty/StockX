955dba334f3903c0dddaa991a63a9b01c40c2934
Manuerty <manuelcendonr@gmail.com>
2024-03-05 13:45:59 +0100

    feat (src) create the Bid class

--- src/main/java/edu/badpals/stockX/offer/Bid.java
+++ src/main/java/edu/badpals/stockX/offer/Bid.java
@@ -1,7 +1,24 @@
 package edu.badpals.stockX.offer;
 
-public class Bid {
+public class Bid implements Offer{
 
-        private String size;
-        private Integer bid;
+        private String size="";
+        private Integer bid=0;
+
+        Bid(String size, Integer bid){
+                this.size= size;
+                this.bid = bid;
+        }
+        public String size(){
+                return  this.size;
+        }
+        public int value(){
+                return  this.bid;
+        }
+        public int compareTo(Offer bid){
+                return this.bid.compareTo(bid.value());
+        }
+        public String toString(){
+                return this.size() + Integer.toString(this.value());
+        }
 }
