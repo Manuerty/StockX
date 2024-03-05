48275831f55ee04611c6039cf32aa68b8abfd10e
Manuerty <manuelcendonr@gmail.com>
2024-03-05 13:27:13 +0100

    refactor (src) change the nameclass and crate the constructor

--- src/main/java/edu/badpals/stockX/offer/ask.java
+++ src/main/java/edu/badpals/stockX/offer/ask.java
@@ -1,4 +1,11 @@
 package edu.badpals.stockX.offer;
 
-public class ask {
+public class Ask {
+    private String size;
+    private Integer ask;
+
+    Ask(String size, Integer ask){
+        this.ask = ask;
+        this.size = size;
+    }
 }
