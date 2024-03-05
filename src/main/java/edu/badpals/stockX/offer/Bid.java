808c865562ab13a96aa9f6140ddf063a2ca7f517
Manuerty <manuelcendonr@gmail.com>
2024-03-05 13:56:01 +0100

    refactor (src) add notes for the future

--- src/main/java/edu/badpals/stockX/offer/Bid.java
+++ src/main/java/edu/badpals/stockX/offer/Bid.java
@@ -9,15 +9,19 @@ public class Bid implements Offer{
                 this.size= size;
                 this.bid = bid;
         }
+        @Override
         public String size(){
                 return  this.size;
         }
+        @Override
         public int value(){
                 return  this.bid;
         }
+        @Override
         public int compareTo(Offer bid){
                 return this.bid.compareTo(bid.value());
         }
+        @Override
         public String toString(){
                 return this.size() + Integer.toString(this.value());
         }
