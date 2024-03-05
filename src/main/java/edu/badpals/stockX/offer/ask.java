2398c35fb9e4a70f5b9b7eede89da393fe557644
Manuerty <manuelcendonr@gmail.com>
2024-03-05 13:39:28 +0100

    feat (src) create ask class

--- src/main/java/edu/badpals/stockX/offer/ask.java
+++ src/main/java/edu/badpals/stockX/offer/ask.java
@@ -1,11 +1,28 @@
 package edu.badpals.stockX.offer;
 
-public class Ask {
-    private String size;
-    private Integer ask;
+public class Ask implements Offer{
+    private String size="";
+    private Integer ask= 0;
 
     Ask(String size, Integer ask){
         this.ask = ask;
         this.size = size;
+    }
+    @Override
+    public String size(){
+        return this.size;
+    }
+    @Override
+    public int value(){
+        return this.ask;
+    }
+
+    @Override
+    public int compareTo(Offer ask) {
+        return this.ask.compareTo(ask.value());
+    }
+
+    public String toString(){
+        return  this.size() +  this.value() ;
     }
 }
